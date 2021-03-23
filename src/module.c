7f99278b8bc816c684999b12c01382d37e7666ba
Andrew Palmer <atp@sdf.org>
2021-03-23 12:57:25 -0500

    Add module method to create Session objects

--- src/module.c
+++ src/module.c
@@ -1,12 +1,40 @@
 #include <Python.h>
 #include "easyadapter.h"
+#include "requests.h"
+
+static PyObject *
+_CurlEasySession(PyObject *self, PyObject *args, PyObject *kwargs)
+{
+    PyObject *adapter = CurlEasyAdapter_New(args, kwargs);
+    PyObject *result = RequestsMod_Session_New();
+
+    PyObject *mount = PyUnicode_FromString("mount");
+    PyObject *http = PyUnicode_FromString("http://");
+    PyObject *https = PyUnicode_FromString("https://");
+
+    PyObject *httpsargs[] = {result, https, adapter};
+    PyObject_VectorcallMethod(mount, httpsargs, 3, NULL);
+    PyObject *httpargs[] = {result, http, adapter};
+    PyObject_VectorcallMethod(mount, httpargs, 3, NULL);
+
+    Py_DECREF(mount);
+    Py_DECREF(http);
+    Py_DECREF(https);
+
+    return result;
+}
+
+static PyMethodDef methods[] = {
+    {"CurlEasySession", (PyCFunction)_CurlEasySession, METH_VARARGS | METH_KEYWORDS, ""},
+    {0},
+};
 
 struct PyModuleDef module_def = {
     .m_base = PyModuleDef_HEAD_INIT,
     .m_name = "requests_curl",
     .m_doc = "",
     .m_size = 0,
-    .m_methods = NULL,
+    .m_methods = methods,
 };
 
 PyMODINIT_FUNC
