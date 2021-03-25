722b625545c36cf78708ed7b76687650a7220c17
Andrew Palmer <atp@sdf.org>
2021-03-24 22:44:54 -0500

    Clean up unused util function

--- src/util.c
+++ src/util.c
@@ -11,22 +11,6 @@ util_dict_pop(PyObject *dict, const char *key)
     return result;
 }
 
-PyObject *
-util_dict_pick_off(PyObject *dict, const char *keys[])
-{
-    PyObject *result = PyDict_New();
-    const char *k = keys[0];
-    while (k) {
-        PyObject *val = util_dict_pop(dict, k);
-        if (val) {
-            PyDict_SetItemString(dict, k, val);
-            Py_DECREF(val);
-        }
-        ++k;
-    }
-    return result;
-}
-
 void
 util_pick_off_keywords(PyObject *kwdict, const char *kwlist[], ...)
 {
--- src/util.h
+++ src/util.h
@@ -4,7 +4,6 @@
 #include <Python.h>
 
 PyObject *util_dict_pop(PyObject *dict, const char *key);
-PyObject *util_dict_pick_off(PyObject *dict, const char *keys[]);
 PyObject *util_pick_off_keywords(PyObject *kwargs, const char *kwlist[], ...);
 
 #endif
