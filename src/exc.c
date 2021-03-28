add65122141b4af3f11b07c0107ef416c8c3cf38
Andrew Palmer <atp@sdf.org>
2021-03-28 16:09:46 -0500

    Refactor exc_set_from_CURLcode

--- src/exc.c
+++ src/exc.c
@@ -1,12 +1,28 @@
 #include <curl/curl.h>
 #include "requests.h"
 
-void
-exc_set_from_CURLcode(CURLcode code, const char *errorbuffer, PyObject *request, PyObject *response)
+/* TODO: move to util */
+PyObject *
+util_obj_BuildAttrString(PyObject *o, const char *name, const char *fmt, ...)
 {
-    PyObject *msg = NULL;
-    PyObject *args = NULL;
-    PyObject *kwargs = NULL;
+    va_list va;
+    va_start(va, fmt);
+    PyObject *value = Py_VaBuildValue(fmt, va);
+    va_end(va);
+
+    if (!value)
+        return NULL;
+
+    PyObject *result = PyObject_SetAttrString(o, name, value);
+
+    Py_DECREF(value);
+
+    return result;
+}
+
+static PyObject *
+_exctype_from_CURLcode(CURLcode code)
+{
     PyObject *exctype = NULL;
 
     switch (code) {
@@ -21,8 +37,16 @@ exc_set_from_CURLcode(CURLcode code, const char *errorbuffer, PyObject *request,
         break;
     }
 
-    if (!exctype)
-        goto out;
+    return exctype;
+}
+
+static PyObject *
+_excobj_new(PyObject *exctype, CURLcode code, const char *errorbuffer, PyObject *request, PyObject *response)
+{
+    PyObject *msg = NULL;
+    PyObject *args = NULL;
+    PyObject *kwargs = NULL;
+    PyObject *excobj = NULL;
 
     msg = PyUnicode_FromFormat("%s (CURLcode: %ld)", curl_easy_strerror(code), code);
     if (!msg)
@@ -34,17 +58,33 @@ exc_set_from_CURLcode(CURLcode code, const char *errorbuffer, PyObject *request,
     if (!kwargs)
         goto out;
 
-    PyObject *excobj = PyObject_Call(exctype, args, kwargs);
+    excobj = PyObject_Call(exctype, args, kwargs);
     if (!excobj)
         goto out;
-    PyObject_SetAttrString(excobj, "curl_code", PyLong_FromLong(code));
-    PyObject_SetAttrString(excobj, "curl_errorbuffer", PyUnicode_FromString(errorbuffer));
-    PyErr_SetObject(exctype, excobj);
-    Py_DECREF(excobj);
+
+    util_obj_BuildAttrString(excobj, "curl_code", "i", code);
+    util_obj_BuildAttrString(excobj, "curl_strerror", "s", curl_easy_strerror(code));
+    util_obj_BuildAttrString(excobj, "curl_errorbuffer", "s", errorbuffer);
 
 out:
     Py_XDECREF(msg);
     Py_XDECREF(args);
     Py_XDECREF(kwargs);
+    return excobj;
+}
+
+void
+exc_set_from_CURLcode(CURLcode code, const char *errorbuffer, PyObject *request, PyObject *response)
+{
+    PyObject *exctype = _exctype_from_CURLcode(code);
+    if (!exctype)
+        return;
+
+    PyObject *excobj = _excobj_new(exctype, code, errorbuffer, request, response);
+    if (!excobj)
+        return;
+
+    PyErr_SetObject(exctype, excobj);
+    Py_DECREF(excobj);
 }
 
